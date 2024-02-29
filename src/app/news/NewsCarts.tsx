3fb4ab6dd465817542c57f48bccf41162f7039c3
Dilshod-Abdullayev <dilshodcoder6636@gmail.com>
2024-02-29 20:21:33 +0500

    news page start

---
+++ src/app/news/NewsCarts.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NewsCarts() {
+    return (
+        <div>NewsCarts</div>
+    )
+}
---
+++ src/app/news/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NewsHeader from './NewsHeader'
+
+export default function page() {
+  return (
+    <div className='mt-4'>
+      <NewsHeader/>
+    </div>
+  )
+}
