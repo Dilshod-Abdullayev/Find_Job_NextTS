f25d2b0a475b85f1e7a77f58dea7a5ee04299e8c
Dilshod-Abdullayev <dilshodcoder6636@gmail.com>
2024-05-16 15:51:31 +0500

    filter  start

--- src/app/saves/page.tsx
+++ src/app/saves/page.tsx
@@ -1,11 +1,7 @@
 "use client"
 import React from 'react'
-import { FaBusinessTime } from "react-icons/fa";
-import { SiExpertsexchange } from "react-icons/si";
-import { MdOnlinePrediction, MdCancel } from "react-icons/md";
-import { FaLocationDot } from "react-icons/fa6";
 import { useSelector } from "react-redux";
-import Link from 'next/link';
+import JobCard from '../ishlar/JobCard';
 export default function page() {
     interface RootState {
         savejob: {
@@ -29,41 +25,11 @@ export default function page() {
     return (
         <div className='flex w-full justify-around p-4  flex-wrap gap-2'>
             {
-                savejob.map((item) => (
-                    <div key={item.id} className='w-60 max-md:w-72 boxshadow h-72 align-middle border border-slate-300 p-2  rounded-xl'>
-                        <div className='flex justify-around items-center'>
-                            <img src="https://play-lh.googleusercontent.com/L3w73ZSvHAU3C6PmPWeuqKEHe67igSj5Ahovxlf0vBSdWi-31_PrLbHBHoSwmzAVzw"
-                                className='w-10 h-10'
-                                alt="" />
-                            <div className='flex flex-col  items-center'>
-                                <h1 className='text-base leading-2 font-semibold'>{item.position}</h1>
-                                <p className='text-sm'>{item.company}</p>
-                            </div>
-                        </div>
-                        <div className='flex text-sky-500 w-full mt-2 justify-between px-2 text-xl font-extrabold'>
-                            <FaBusinessTime className='w-1/3 flex justify-center' />
-                            <SiExpertsexchange className='w-1/3 flex justify-center' />
-                            <MdOnlinePrediction className='w-1/3 flex justify-center' />
-                        </div>
-                        <div className='flex w-full justify-between p-2 text-sm font-extrabold'>
-                            <h1 className='w-1/3 flex justify-center'>{item.timeType}</h1>
-                            <h1 className='w-1/3 flex justify-center'>{item.experiense}</h1>
-                            <h1 className='w-1/3 flex justify-center'>{item.typeJob}</h1>
-                        </div>
-                        <div className='flex gap-2 text-sm text-slate-600 font-extrabold p-2'>
-                            {
-                                item.techno.map((item, index) => (
-                                    <h1 key={index}>{item}</h1 >
-                                ))
-                            }
-                        </div >
-                        <h1 className='flex items-center text-sky-500 font-extrabold'>Location:<FaLocationDot />  <span className='text-slate-900'>{item.location}</span> </h1>
-                        <div className='flex items-center justify-around mt-2'>
-                            <h1 className='text-sky-500 font-extrabold'>{item.salary}$</h1>
-                            <Link href={"ishlar/" + item.id.toString()} className='bg-sky-600 hover:bg-sky-500 text-white rounded-xl  max-md:w-1/2  py-1 px-4'>Qabul qilish</Link>
-                        </div>
-                    </div>
-                ))
+                savejob.length > 0 ? (
+                    savejob.map((item) => (
+                        <JobCard key={item.id} item={item} />
+                    ))
+                ) : <p>No saved jobs found.</p>
             }
         </div >
     )
